bdff3dbb1cf6e5fe143c82ee538233c6c44a6069
Benjamin Böhm <benjamin@Benjamins-MacBook-Pro.local>
2017-01-10 10:43:38 +0100

    Changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'December'];
+    let view = 1;
     function getMonthDays(year, month) {
         let date = new Date(year, month + 1, 0);
         return date.getDate();
@@ -26,8 +28,8 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function buildCalendar(date, today) {
-        let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Oktober', 'November', 'December'];
         let calendarGrid = document.getElementById('calendar-grid');
+        document.getElementById('table-container').style.display = 'block';
         let startDate = getStartDay(date.getFullYear(), date.getMonth());
         if (startDate == 0) {
             startDate = 7;
@@ -51,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     dayNumber = amountOfDaysPrevMonth + dayNumber;
                     td.className = 'old-month not-from-this-month dateField';
                 }
-                if (today.getMonth() == date.getMonth() && today.getDate() == dayNumber && !(hasClass(td, 'new-month')) && !(hasClass(td, 'old-month'))) {
+                if (today.getFullYear == date.getFullYear && today.getMonth() == date.getMonth() && today.getDate() == dayNumber && !(hasClass(td, 'new-month')) && !(hasClass(td, 'old-month'))) {
                     td.className = 'today dateField';
                 }
                 td.innerHTML = dayNumber;
@@ -64,8 +66,9 @@ document.addEventListener("DOMContentLoaded", function() {
         calendarGrid.appendChild(tbl);
         monthYearTitle = monthNames[date.getMonth()] + ' ' + date.getFullYear();
         document.getElementById('month-year').innerHTML = monthYearTitle;
+        view = 1;
 
-        let dateFields = document.getElementsByClassName('dateField');
+        /*let dateFields = document.getElementsByClassName('dateField');
         for (let count = 0; count < dateFields.length; count++) {
             dateField = dateFields[count];
             console.log(dateFields.item(count));
@@ -78,7 +81,68 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             });
         }
+        */
     }
+
+    function showMonthsOverview(year) {
+        tbl = document.createElement('table');
+        for (let i = 0; i < 4; i++) {
+            tr = document.createElement('tr');
+            for (let x = 0; x < 3; x++) {
+                td = document.createElement('td');
+                td.innerHTML = monthNames[x + 3 * i];
+                td.className = 'monthButton';
+                tr.appendChild(td);
+            }
+            tbl.appendChild(tr);
+        }
+        document.getElementById('table-container').style.display = 'none';
+        calendarGrid = document.getElementById('calendar-grid');
+        calendarGrid.innerHTML = '';
+        calendarGrid.appendChild(tbl);
+        document.getElementById('month-year').innerHTML = year;
+        view = 2;
+        let monthButton = document.getElementsByClassName('monthButton');
+        for (let count = 0; count < monthButton.length; count++) {
+            let button = monthButton[count];
+            button.addEventListener('click', function() {
+              let newDate = new Date(year, count, 1);
+              let today = new Date();
+              console.log(newDate);
+              buildCalendar(newDate, today);
+            });
+        }
+    }
+    function showYearsOverview(year){
+      let yearDigits = year.toString().split('');
+      let endyear = year + (10 - yearDigits[3]);
+      let startyear = endyear - 9;
+      tbl = document.createElement('table');
+      for (let i = 0; i < 5; i++) {
+        tr = document.createElement('tr');
+        for (let x = 0; x < 2; x++) {
+          td = document.createElement('td');
+          td.className = 'yearButton';
+          td.innerHTML = startyear + x + 2 * i;
+          tr.appendChild(td);
+        }
+        tbl.appendChild(tr);
+      }
+      document.getElementById('table-container').style.display = 'none';
+      calendarGrid = document.getElementById('calendar-grid');
+      calendarGrid.innerHTML = '';
+      calendarGrid.appendChild(tbl);
+      document.getElementById('month-year').innerHTML = startyear + ' - ' + endyear;
+      view = 3;
+      let yearButton = document.getElementsByClassName('yearButton');
+      for (let count = 0; count < yearButton.length; count++) {
+          let button = yearButton[count];
+          button.addEventListener('click', function() {
+            showMonthsOverview(+(button.innerHTML));
+          });
+      }
+    }
+
     let date = new Date();
     let datepicker = document.getElementById('datepicker');
     let today = new Date();
@@ -87,15 +151,29 @@ document.addEventListener("DOMContentLoaded", function() {
         buildCalendar(date, today);
     }
     buildCalendar(date, today);
-
     let nextButton = document.getElementById('next');
     let prevButton = document.getElementById('prev');
+    let overviewButton = document.getElementById('month-year')
+    let dayButton = document.getElementsByClassName('dayButton');
+    let monthButton = document.getElementsByClassName('monthButton');
+    let centuryButton = document.getElementsByClassName('centuryButton');
     nextButton.addEventListener('click', function() {
         date.setMonth(date.getMonth() + 1);
         buildCalendar(date, today);
     });
     prevButton.addEventListener('click', function() {
+      if(view == 1){
         date.setMonth(date.getMonth() - 1);
         buildCalendar(date, today);
+      }
+    });
+    overviewButton.addEventListener('click', function() {
+      if(view == 1){
+        showMonthsOverview(date.getFullYear());
+      }
+      else if(view == 2){
+        showYearsOverview(date.getFullYear());
+      }
+
     });
 });
